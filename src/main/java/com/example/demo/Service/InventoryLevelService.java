583ebebff1735ad42fc4cbfa9cd6d9a8bb0d8325
bala-amypo <bala@amypo.in>
2025-12-22 03:58:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/InventoryLevelService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.InventoryLevel;
+import java.util.List;
+
+public interface InventoryLevelService {
+
+    InventoryLevel updateInventory(Long storeId, Long productId, Integer quantity);
+
+    List<InventoryLevel> getInventoryByStore(Long storeId);
+}
