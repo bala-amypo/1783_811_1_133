0cef12f06c78bb8a09d628a80915ce18bf616fde
bala-amypo <bala@amypo.in>
2025-12-27 09:56:14 +0000

    User commit

--- src/main/java/com/example/demo/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.util;
-
-public final class Constants {
-
-    private Constants() {}
-
-    public static final String STATUS_MESSAGE =
-            "Multi-Location Inventory Balancer is running";
-}
