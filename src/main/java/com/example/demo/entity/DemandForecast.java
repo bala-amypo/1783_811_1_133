27eb6210a61faa8faee500f5fda2e1e8ee55f84c
bala-amypo <bala@amypo.in>
2025-12-28 05:23:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/DemandForecast.java
+++ src/main/java/com/example/demo/entity/DemandForecast.java
@@ -10,15 +10,59 @@ public class DemandForecast {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     private Store store;
 
-    @ManyToOne(optional = false)
+    @ManyToOne
     private Product product;
 
     private LocalDate forecastDate;
+
     private Integer forecastedDemand;
+
     private Double confidenceScore;
 
-    // getters/setters unchanged
+    public Long getId() {
+        return id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public LocalDate getForecastDate() {
+        return forecastDate;
+    }
+
+    public void setForecastDate(LocalDate forecastDate) {
+        this.forecastDate = forecastDate;
+    }
+
+    public Integer getForecastedDemand() {
+        return forecastedDemand;
+    }
+
+    public void setForecastedDemand(Integer forecastedDemand) {
+        this.forecastedDemand = forecastedDemand;
+    }
+
+    public Double getConfidenceScore() {
+        return confidenceScore;
+    }
+
+    public void setConfidenceScore(Double confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
 }
