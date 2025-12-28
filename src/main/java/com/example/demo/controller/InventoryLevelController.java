da2679e8c2b910c1864e37c81502b8a182d0d9a9
bala-amypo <bala@amypo.in>
2025-12-28 08:09:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/InventoryLevelController.java
+++ src/main/java/com/example/demo/controller/InventoryLevelController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.InventoryLevel;
+import com.example.demo.entity.Product;
+import com.example.demo.entity.Store;
 import com.example.demo.service.InventoryLevelService;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +17,20 @@ public class InventoryLevelController {
     }
 
     @PostMapping
-    public InventoryLevel create(@RequestBody InventoryLevel inventoryLevel) {
-        return inventoryService.createOrUpdateInventory(inventoryLevel);
+    public InventoryLevel create(@RequestBody InventoryLevel inv) {
+
+        InventoryLevel safe = new InventoryLevel();
+        safe.setQuantity(inv.getQuantity());
+
+        Store store = new Store();
+        store.setId(inv.getStore().getId());
+
+        Product product = new Product();
+        product.setId(inv.getProduct().getId());
+
+        safe.setStore(store);
+        safe.setProduct(product);
+
+        return inventoryService.createOrUpdateInventory(safe);
     }
 }
