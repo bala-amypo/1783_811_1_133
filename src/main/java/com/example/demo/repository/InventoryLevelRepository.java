c873c1f9d8835670a9f0862013d9efe43728db6a
bala-amypo <bala@amypo.in>
2025-12-28 06:02:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/InventoryLevelRepository.java
+++ src/main/java/com/example/demo/repository/InventoryLevelRepository.java
@@ -4,13 +4,13 @@ import com.example.demo.entity.InventoryLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface InventoryLevelRepository
         extends JpaRepository<InventoryLevel, Long> {
 
     List<InventoryLevel> findByStore_Id(Long storeId);
-    Optional<InventoryLevel> findByStore_IdAndProduct_Id(Long storeId, Long productId);
+
+    InventoryLevel findByStore_IdAndProduct_Id(Long storeId, Long productId);
 
     List<InventoryLevel> findByProduct_Id(Long productId);
 }
